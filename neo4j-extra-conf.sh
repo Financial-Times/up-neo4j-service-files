f65e7fcda4d2f841a8822943fe83a45c5fb85139
Fernee, Julia <Julia.Fernee@ft.com>
2017-01-30 14:26:23 +0000

    added logging to neo4j config

--- neo4j-extra-conf.sh
+++ neo4j-extra-conf.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+#Neo4J logging
+    setting "dbms.logs.http.enabled" true
+    setting "dbms.logs.query.enabled" true
+    setting "dbms.logs.query.parameter_logging_enabled" true
+
 # Graphite integration start
 if [ "${GRAPHITE_ENABLED:-false}" = true ]; then
     setting "metrics.enabled" "${GRAPHITE_ENABLED}"
