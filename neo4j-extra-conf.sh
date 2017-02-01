df96e1c50a9ce42d72ef13b7684b732154c7c247
JuliaFernee <julia.fernee@ft.com>
2017-02-01 16:40:17 +0000

    Add services (#1)
    * added intelliJ conf to gitignore
    
    * copied relevant service files AS IS from up-service-files
    
    * WIP - added neo4j service files
    
    * modified service files for neo4j deployment
    
    * Looks like deployer needs sidekick no matter what.
    
    * added info about neo4j cluster deploymnet in README.
    
    * improved README added note about cluster decom.
    
    * removed ref to 'blue' config and replaced 1 with %i for concistency in neo4j-hot-backup and ne4j.service
    
    * added envs that will be needed to run neo4j image with new configuration.
    
    * commented out all extra ens to debug failing deployer
    
    * fixed slashes in neo4j.service
    
    * another fix to envs because deployer fails
    
    * envs didn't get into docker - trying again
    
    * envs don't get passed correctly -trying again
    
    * still not writing all envs - trying again
    
    * mapped ports and added param for causal deployment.
    
    * forgot backslash
    
    * Added -neo4j- to the tunnel DNS name to avoid conflicts with the main cluster
    
    * Bumped to rc2
    
    * Fixed data path, changed  ADVERTISED_ADDRESS to upper case and formatting.
    
    * Make entrypoint executable
    
    * Removed braces
    
    * Removed whitespace
    
    * Added correct memory options
    
    * Use hostname instead of IP
    
    * Mapped data to data folder
    
    * Bind neo4j to host network
    
    * Trying with official Neo4J Docker image
    
    * Fixed link to old image
    
    * Tunnel uses private IP
    
    * Use proper local ipv4
    
    * added logging to neo4j config
    
    * fixed syntax
    
    * fixung settings that arn't written to the heo4j.conf
    
    * fixed error in filiename that caused extra-conf.sh settings not loading.
    
    * setting to write logs to EBS and adjustments to logging configuration - to not keep archives, reduce size and set threshold.
    
    * neo4j is not happy with 0 value for keep_number for log files
    
    * in theory we shouldn't need setting() func as it is defined in the docker-entrypoint.sh of the neo4j docker distribution.
    
    * reduced outpud from debug.log
    
    * fixed
    
    * Sorted logging directory
    
    * make splunk-forwarder read from log files
    
    * removed restrictions on debug.log output because we exclude it from slunk differently.

---
+++ neo4j-extra-conf.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Neo4j http logging
+    setting "dbms.logs.http.enabled" "true"
+    setting "dbms.logs.http.rotation.keep_number" "1"
+    setting "dbms.logs.http.rotation.size" "1m"
+#Neo4j querry logging
+    setting "dbms.logs.query.enabled" "true"
+    setting "dbms.logs.query.parameter_logging_enabled" "true"
+    setting "dbms.logs.query.rotation.keep_number" "1"
+    setting "dbms.logs.query.rotation.size" "1m"
+    setting "dbms.logs.query.threshold" "100ms"
+
+# Graphite integration start
+if [ "${GRAPHITE_ENABLED:-false}" = true ]; then
+    setting "metrics.enabled" "${GRAPHITE_ENABLED}"
+    setting "metrics.graphite.enabled" "${GRAPHITE_ENABLED:-true}"
+    setting "metrics.graphite.server" "${GRAPHITE_ADDRESS}"
+    setting "metrics.graphite.interval" "${GRAPHITE_INTERVAL:-3m}"
+    setting "metrics.prefix" "${GRAPHITE_PREFIX}"
+fi
+# Graphite integration end
+
